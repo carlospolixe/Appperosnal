193dd5c80af368f1bc577c78ec0b17a8738e2f7e
Carlos Diaz <Carlosdiazjimenez0@gmail.co>
2019-11-06 13:40:40 +0100

    commit final proyecto

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
+ 
 import { HomePage } from './home.page';
-
+ 
+import { HttpClientModule } from '@angular/common/http'; 
+ 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
     RouterModule.forChild([
       {
         path: '',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from  '@angular/common/http';
+import { map }  from  'rxjs/operators';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  sex: any;
+  users: any;
 
-  constructor() {}
-
+  
+  constructor(private httpClient: HttpClient) {
+    this.users = this.httpClient.get('https://raw.githubusercontent.com/carlospolixe/json/master/generated.json').pipe(map(res => res['results']));
+  }
 }
